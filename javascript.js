2536e0dc736536b5f0b99b39ddd7746b6fa49fa5
otherdcn <danny.c.ntwari@gmail.com>
2024-04-18 16:09:18 +0200

    Add function to randomly choose computer's choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
-console.log("Outside lookin in!")
+console.log("Hi there!");
+console.log("I'm Lee, the computer :-D!");
+console.log("Let's play a game of Rock, Papers, Scissors!");
+console.log("--------------------------------------------");
+
+const choices = ['rock','paper','scissors'];
+
+function getComputerChoice() {
+  // const choices = ['rock','paper','scissors'];
+  const computerSelection = Math.floor(Math.random()*choices.length);
+  return choices[computerSelection];
+}
+
+console.log(`Computer chose: ${getComputerChoice()}`);
