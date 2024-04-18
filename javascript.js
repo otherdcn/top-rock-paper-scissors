69799f8c09760d30e53c5dc9d82eb69545712c42
otherdcn <danny.c.ntwari@gmail.com>
2024-04-18 18:26:31 +0200

    Add function to play a round and find a winner

--- javascript.js
+++ javascript.js
@@ -1,23 +1,24 @@
 console.log("Hi there!");
 console.log("I'm Lee, the computer :-D!");
 console.log("Let's play a game of Rock, Papers, Scissors!");
+
 console.log("--------------------------------------------");
 
 const choices = ['rock','paper','scissors'];
 
-function getplayerChoice() {
+function getPlayerChoice() {
   let playerSelection;
   let wrongInput = false;
 
   do {
-    const playerInput = prompt("Rock? Paper? Scissor?");
+    const playerInput = prompt("Rock? Paper? Scissors?");
 
     playerSelection = playerInput.toLowerCase();
 
-    if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissor') {
+    if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
       wrongInput = false;
     } else {
-      alert(`Unkown input or misspelling (${playerInput}). Please type Rock, Paper or Scissor.`);
+      alert(`Unkown input or misspelling (${playerInput}). Please type Rock, Paper or Scissors.`);
       wrongInput = true;
     }
 
@@ -31,8 +32,34 @@ function getComputerChoice() {
   return choices[computerSelection];
 }
 
-const playerSelection = getplayerChoice();
+const playerSelection = getPlayerChoice();
 const computerSelection = getComputerChoice();
 
 console.log(`You chose: ${playerSelection}`);
 console.log(`Lee chose: ${computerSelection}`);
+
+function playRound(playerSelection,computerSelection) {
+
+  const possibleOutcomes = [['rock','rock','draw'],
+                 ['rock','paper','lose'],
+                 ['rock','scissors','win'],
+                 ['paper','paper','draw'],
+                 ['paper','scissors','lose'],
+                 ['paper','rock','win'],
+                 ['scissors','scissors','draw'],
+                 ['scissors','rock','lose'],
+                 ['scissors','paper','win']
+                ];
+
+  for (let i = 0; i < possibleOutcomes.length; i++) {
+    if (playerSelection === possibleOutcomes[i][0] && computerSelection === possibleOutcomes[i][1]) {
+      return `You ${possibleOutcomes[i][2]}!`;
+    }
+  }
+}
+
+console.log("--------------------------------------------");
+
+const result = playRound(playerSelection,computerSelection);
+
+console.log(result);
